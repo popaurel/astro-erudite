1c4b0f51b70e4fef67f6fd469b98992c61095e92
Aurel Pop <47039993+popaurel@users.noreply.github.com>
2025-08-11 07:10:08 +0000

    Update footer and cookie consent messages
    - Enhanced the footer by adding links to the Privacy Policy and Terms and Conditions.
    - Updated cookie consent modal title and description for clarity and improved user experience.
    - Adjusted footer links in the cookie consent configuration to point to the correct sections.

--- src/scripts/cookieconsent.js
+++ src/scripts/cookieconsent.js
@@ -30,12 +30,12 @@ CookieConsent.run({
         translations: {
             en: {
                 consentModal: {
-                    title: "Hello traveller, it's cookie time!",
-                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+                    title: "Hello smart investor, it's cookie time!",
+                    description: 'We use essentialy cookies to make our site work. With your consent, we may also use non-essential cookies to improve user experience and analyze website traffic. For these reasons, we may share your site usage data with our analytics partners. By clicking "Accept all" you agree to our website\'s cookie use as described in our <a href=\"#privacy\">Privacy Policy</a>. You can change your cookie settings at any time by clicking "Manage preferences".',
                     acceptNecessaryBtn: "Reject all",
                     acceptAllBtn: "Accept all",
                     showPreferencesBtn: "Manage preferences",
-                    footer: "<a href=\"#link\">Privacy Policy</a>\n<a href=\"#link\">Terms and conditions</a>"
+                    footer: "<a href=\"#privacy\">Privacy Policy</a>\n<a href=\"#terms\">Terms and conditions</a>"
                 },
                 preferencesModal: {
                     title: "Consent Preferences Center",
@@ -47,22 +47,22 @@ CookieConsent.run({
                     sections: [
                         {
                             title: "Cookie Usage",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                            description: "We use different types of cookies to optimize your experience on our website. Click on the categories below to learn more about their purposes. You may choose which types of cookies to allow and can change your preferences at any time. You can learn more about how we use cookies by visiting our <a href=\"#privacy\">Privacy Policy</a>.",
                         },
                         {
                             title: "Strictly Necessary Cookies <span class=\"pm__badge\">Always Enabled</span>",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            description: "These cookies are necessary to the core functionality of our website and some of its features.",
                             linkedCategory: "necessary"
                         },
-                        {
-                            title: "Analytics Cookies",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                            linkedCategory: "analytics"
-                        },
-                        {
-                            title: "More information",
-                            description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
-                        }
+                        // {
+                        //     title: "Analytics Cookies",
+                        //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                        //     linkedCategory: "analytics"
+                        // },
+                        // {
+                        //     title: "More information",
+                        //     description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
+                        // }
                     ]
                 }
             }
